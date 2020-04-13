5d6ffe2416d5a0cac85a5743acab1113ad95a728
Peter Laffey <peter.laffey@gmail.com>
2020-04-13 09:08:02 -0500

    Add code along

---
+++ src/app/courses/reducers/course.reducers.ts
@@ -0,0 +1,44 @@
+import {compareCourses, Course} from '../model/course';
+import {createEntityAdapter, EntityState} from '@ngrx/entity';
+import {createReducer, on} from '@ngrx/store';
+import {CourseActions} from '../action-types';
+
+
+export interface CoursesState extends EntityState<Course> {
+    allCoursesLoaded: boolean
+}
+
+
+export const adapter = createEntityAdapter<Course>({
+    sortComparer: compareCourses
+    // selectId: course => course.courseId  ** This is used when an entity uses a non-standard 'id', such as 'courseId', for the id.  
+    // The mapping is handled here
+});
+
+
+export const initialCoursesState = adapter.getInitialState({
+    allCoursesLoaded:false
+});
+
+
+export const coursesReducer = createReducer(
+
+    initialCoursesState,
+
+    on(CourseActions.allCoursesLoaded,
+        (state, action) => adapter.addAll(
+            action.courses,
+            {...state,
+                allCoursesLoaded:true
+            })),
+
+
+    on(CourseActions.courseUpdated, (state, action) =>
+        adapter.updateOne(action.update, state) )
+
+);
+
+
+export const {
+    selectAll
+} = adapter.getSelectors();
